c93ab2a2e0d13077e65eb871938dc831747e2304
luiz-eduardooo <luizeduardo2013051@gmail.com>
2026-03-24 12:57:51 -0300

    full commit

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import "../css/header.css";
 import logoPng from "../img/logo.png";
