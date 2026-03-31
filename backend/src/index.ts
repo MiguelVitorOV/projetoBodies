24e469384145225773bfd656d4849b23863a1397
MiguelVitorOV <miguelvitor20070@gmail.com>
2026-03-31 11:58:31 -0300

    feat: add cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,11 @@ import path from "path";
 AppDataSource.initialize().then(() => {
     const app = express();
 
-    app.use(cors()); 
+    app.use(cors({
+  origin: '', // O '' libera acesso para QUALQUER site. Ideal para a correria da entrega de amanhã!
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
     // O MESMO CAMINHO DO MULTER (Saindo do backend e indo pra uploads)
     const caminhoDasImagens = path.resolve(process.cwd(), "..", "uploads");
